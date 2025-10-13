0f9b4db5f09065d438e2fd1a9365e7d1ee16f4d9
Veerendra C <chandrashekarveerendra@gmail.com>
2025-10-13 23:20:21 +0530

    Added code to connect to MinIo local server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,12 +5,14 @@ import (
 	"github.com/Veerendra-C/SV-Backend.git/Internal/config"
 	"github.com/Veerendra-C/SV-Backend.git/Internal/db"
 	https "github.com/Veerendra-C/SV-Backend.git/Internal/http"
+	"github.com/Veerendra-C/SV-Backend.git/Internal/storage"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 	
 	logger.InitLogger(cfg.Env)
+	storage.InitMinio()
 	db.InitDB(cfg.Env)
 
 	https.StartServer(cfg)
